9f7d653b15db0cfaeea0025d8e3b1d4c6f973082
Niels Eppenhof <n.eppenhof@gmail.com>
2023-09-18 09:14:37 +0200

    First API call working

--- backend-main/Program.cs
+++ backend-main/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Cors
+builder.Services.AddCors(o => o.AddPolicy("Cors", builder =>
+{
+	builder.AllowAnyOrigin();
+	builder.AllowAnyMethod();
+	builder.AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.UseCors("Cors");
 
 app.UseHttpsRedirection();
 
